661f3819f6466414e1338685b552b818d08b714e
Olena Derevianko <olenka.derevianko@gmail.com>
2022-12-29 14:07:01 +0100

    add hw2.2

---
+++ src/components/App.jsx
@@ -0,0 +1,71 @@
+import { GlobalStyle } from "./GlobalStyle";
+import { Component } from "react";
+import { Section } from "./Section/Section";
+import { ContactForm } from "./ContactForm/ContactForm";
+import { Filter } from "./Filter/Filter";
+import { ContactList } from "./ContactList/ContactList";
+import shortid from "shortid";
+
+export class App extends Component{
+  
+state = {
+  contacts: [
+    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+  ],
+  filter: '',
+  }
+  
+    addContact = ({ name, number }) => {
+    const contact = {
+      id: shortid.generate(),
+      name,
+      number,
+    };
+    const { contacts } = this.state;
+    const isExistContact = contacts.find(
+      contact => contact.name.toLowerCase() === name.toLowerCase()
+    );
+    if (isExistContact) {
+      alert(`${name} is already in contacts`);
+      return;
+    }
+    this.setState(({ contacts }) => ({
+      contacts: [contact, ...contacts],
+    }));
+  };
+
+  deleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
+  }
+
+ changeFilter = event => {
+    this.setState({filter: event.currentTarget.value});
+  };
+  
+  render() {
+
+    const visibleContacts = this.state.contacts.filter(abonent =>
+      abonent.name.toLowerCase().includes(this.state.filter)
+    );
+
+    return (      
+      <>
+        <Section title="Phonebook">
+          <ContactForm onSubmit={this.addContact} />
+        </Section>
+
+        <Section title="Contacts">
+          <Filter value={this.state.filter} onChange={this.changeFilter } />
+          <ContactList listContacts={visibleContacts} onDelete = {this.deleteContact} />
+        </Section>
+
+        <GlobalStyle />
+    </>
+    );
+  }
+};
